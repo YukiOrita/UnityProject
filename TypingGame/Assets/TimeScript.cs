5e811403139ffaabbd143c95cd38dfc17f07871b
折田優紀 <oritayuki@oritanatsukinoMacBook-Pro.local>
2017-01-26 20:29:13 +0900

    プロジェクトファイル追加

---
+++ TypingGame/Assets/TimeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScript : MonoBehaviour {
+
+	public TextMesh timeTextMesh;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TypingGame/Assets/TypingObject.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TypingObject : MonoBehaviour {
+
+	public TextMesh InputStringTextMesh;
+	public TextMesh AlphabetTextMesh;
+	TypingSystem ts;
+	public Dictionary dictionary;
+
+	// Use this for initialization
+	void Start () {
+
+		ts = new TypingSystem ();
+		ts.SetInputString (dictionary.GetRandomWord());
+		UpdateText ();
+	}
+	
+	// Update is called once per frame
+	void Update (){
+
+		string[] keys = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-",};
+		foreach (string key in keys) {
+			if(Input.GetKeyDown(key)){
+				if (ts.InputKey (key) == 1) {
+					UpdateText ();
+				}
+				break;
+	}
+		}
+		if (ts.isEnded ()) {
+			ts.SetInputString (dictionary.GetRandomWord());
+			UpdateText ();
+		}
+	}
+
+	void UpdateText () {
+
+		InputStringTextMesh.text =  "<color=red>" + ts.GetInputedString() + "</color>" + ts.GetRestString();
+		AlphabetTextMesh.text = "<color=red>" + ts.GetInputedKey() + "</color>" + ts.GetRestKey();
+	}
+
+}
+
